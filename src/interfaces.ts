6d8df67769d2d4d79248a3d17686cf2b5ba86fd8
Nitin Jhamtani <nitinjhamtani3@gmail.com>
2022-03-13 23:03:44 +0530

    Initial commit

---
+++ src/interfaces.ts
@@ -0,0 +1,15 @@
+export interface Employee {
+  uniqueId: number;
+  name: string;
+  subordinates: Employee[];
+}
+
+export interface IEmployeeOrgApp {
+  orgChart: Employee;
+
+  move(employeeID: number, supervisorID: number): void;
+
+  undo(): void;
+
+  redo(): void;
+}
